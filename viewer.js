86ce10180b9bf8a9908bd27c120d379aaec3d067
seiya1110 <seiya.ozaki@gmail.com>
2024-12-10 22:48:16 +0900

    Add files via upload

---
+++ viewer.js
@@ -0,0 +1,26 @@
+var pictNo = 1;
+
+function revBtnClick(){
+  pictNo--;
+  document.getElementById('no').innerHTML = 'No.' + pictNo;
+  document.getElementById('pict').src = 'img/' + pictNo + '.jpg';
+  if (pictNo === 1){
+    document.getElementById('rev_btn').disabled = 'disabled';
+  } else if (pictNo === 3){
+    document.getElementById('fwd_btn').disabled = false;
+    document.getElementById('no').className = 'no1';
+  }
+}
+
+function fwdBtnClick(){
+  pictNo++;
+  document.getElementById('no').innerHTML = 'No.' + pictNo;
+  document.getElementById('pict').src = 'img/' + pictNo + '.jpg';
+  
+  if (pictNo === 4){
+    document.getElementById('no').className = 'no2';
+    document.getElementById('fwd_btn').disabled = 'disabled';
+  } else if (pictNo === 2){
+    document.getElementById('rev_btn').disabled = false;
+  }
+}
